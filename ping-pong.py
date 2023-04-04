9635324bb03f759b8a31eef304851f863a1b4d6a
ArtemSevcov1769 <artemsevcov349@gmail.com>
2023-04-04 12:26:57 +0500

    1.0.0

--- ping-pong.py
+++ ping-pong.py
@@ -1,11 +1,10 @@
 from pygame import *
 FPS = 60
 
-bg_color = (0, 255, 0)
 WIDTH, HEIGHT = 600, 400
 mw = display.set_mode((WIDTH, HEIGHT))
+back_img = 'pixil-frame-0 (4).png'
 display.set_caption('Ping_pong')
-mw.fill(bg_color)
 class GameSprite(sprite.Sprite):
     def __init__(self, sprite_img, x=0, y=0, w=50, h=50):
         self.image = transform.scale(image.load(sprite_img), (w, h))
@@ -14,10 +13,12 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
     def reset(self):
         mw.blit(self.image, (self.rect.x, self.rect.y))
+
+background = GameSprite(back_img, 0, 0, 600, 400)
 game = True
 clock = time.Clock()
 while game:
-
+    background.reset()
     for e in event.get():
         if e.type == QUIT:
             game = False
