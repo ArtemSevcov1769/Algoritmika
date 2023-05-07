0d5148e79c5bac6abb7c9af5442b35c81f466fea
ArtemSevcov1769 <103888435+ArtemSevcov1769@users.noreply.github.com>
2023-05-07 13:23:00 +0300

    Update ping-pong.py
    Algo

--- Ping-Pong/ping-pong.py
+++ Ping-Pong/ping-pong.py
@@ -1,5 +1,6 @@
 from pygame import *
-
+font.init()
+from Button import Button
 FPS = 60
 WIDTH, HEIGHT = 600, 400
 mw = display.set_mode((WIDTH, HEIGHT))
@@ -7,7 +8,7 @@ back_img = 'pixil-frame-0 (4).png'
 platform_img = 'platforn.png'
 ball_img = 'ball.png'
 display.set_caption('Ping_pong')
-
+stage = 'menu'
 
 class GameSprite(sprite.Sprite):
     def __init__(self, sprite_image, x=0, y=0, w=50, h=50):
@@ -48,14 +49,22 @@ class Ball(GameSprite):
     def player_collide(self, player):
         if sprite.collide_rect(self, player):
             self.dx *= -1
+btn_start = Button(y=200, width=150, height=40, text='Начать игру', font_size=26)
+btn_credits = Button(y=250, width=150, height=40, text='Разработчики', font_size=26)
+btn_exit = Button(y=300, width=150, height=40, text='Выход', font_size=26)
+
+btn_continue = Button(y=200, width=150, height=40, text='Продолжить', font_size=26)
+btn_to_menu = Button(y=250, width=150, height=40, text='Вернуться в меню', font_size=26)
+
 platform_1 = Player(platform_img, 10, HEIGHT/2, 15, 90, 5, K_w, K_s)
 platform_2 = Player(platform_img, 570, HEIGHT/2, 15, 90, 5, K_UP, K_DOWN)
 ball = Ball(ball_img, WIDTH/2, HEIGHT/4, 32, 32, 3)
-background = GameSprite(back_img, 0, 0, 600, 400)
-game = True
+background = transform.scale(image.load(back_img), (WIDTH, HEIGHT))
+
 clock = time.Clock()
-while game:
-    background.reset()
+
+def game():
+    mw.blit(background, (0, 0))
     platform_1.update()
     platform_2.update()
     ball.update()
@@ -64,9 +73,53 @@ while game:
     platform_2.reset()
     ball.player_collide(platform_1)
     ball.player_collide(platform_2)
-    for e in event.get():
+
+def menu(events):
+    global stage
+    mw.blit(background, (0, 0))
+    btn_start.update(events)
+    btn_credits.update(events)
+    btn_exit.update(events)
+    btn_start.draw(mw)
+    btn_credits.draw(mw)
+    btn_exit.draw(mw)
+    if btn_exit.is_clicked(events):
+        stage = 'off'
+    if btn_start.is_clicked(events):
+        restart()
+        stage = 'game'
+def pause(events):
+    mw.blit(background, (0, 0))
+    btn_continue.update(events)
+    btn_to_menu.update(events)
+    btn_continue.draw(mw)
+    btn_to_menu.draw(mw)
+    global stage
+    if btn_continue.is_clicked(events):
+        stage = 'game'
+    if btn_to_menu.is_clicked(events):
+        stage = 'menu'
+def restart():
+    global platform_1, platform_2, ball
+    platform_1 = Player(platform_img, 10, HEIGHT/2, 15, 90, 5, K_w, K_s)
+    platform_2 = Player(platform_img, 570, HEIGHT/2, 15, 90, 5, K_UP, K_DOWN)
+    ball = Ball(ball_img, WIDTH/2, HEIGHT/4, 32, 32, 3)
+
+while stage != 'off':
+    events = event.get()
+    for e in events:
         if e.type == QUIT:
-            game = False
+            stage = 'off'
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                stage = 'pause' 
+
+    if stage == 'menu':
+        menu(events)
+    elif stage == 'game':
+        game()
+    elif stage == 'pause':
+        pause(events)
     display.update()
     clock.tick(FPS)
     
